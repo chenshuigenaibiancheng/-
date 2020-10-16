6117be3b1307eb35c92949a650469b48a1a0066b
陈水根 <72959808+chenshuigenaibiancheng@users.noreply.github.com>
2020-10-16 12:55:08 +0800

    Add files via upload

---
+++ kkkk.c
@@ -0,0 +1,40 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+//{
+//	int day = 0;
+//	scanf ()
+//	return 0;
+//}
+
+//{
+//	int day = 0;
+//	scanf("%d", &day);
+//	if (1 == day);
+//	printf("ÐÇÆÚ1\n", day);
+//	return 0;
+//}
+
+
+
+
+
+//{	1		1
+//	int i = 1;
+//	while (i <= 100)
+//	{
+//		printf("%d ", i);
+//		i += 2;
+//	}
+//	return 0;
+//}
+//{
+//	////int i = 1;
+//	while (i <= 100)
+//	{
+//		if (i % 2 == 1)
+//			printf(" %d ", i);
+//		i++;
+//
+//	}return 0;
+//}
